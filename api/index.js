671e6ba99a2ab9e33818d308c4be690538bd7f36
aayush-rai12 <ayush.wrk1212@gmail.com>
2024-10-21 08:40:16 +0530

    coonected to mongo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
-import express from 'express';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MANGO)
+  .then(() => {
+    console.log("connnected to db");
+  })
+  .catch((err) => {
+    console.log("not connected");
+  });
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('server is runnnig on port 3000!!!!!1');
+app.listen(3000, () => {
+  console.log("server is runnnig on port 3000!!!!!1");
 });
